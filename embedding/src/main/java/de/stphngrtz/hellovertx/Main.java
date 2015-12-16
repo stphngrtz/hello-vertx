5d53cdca57b92cd8d337f73797fbffae1931a7a4
stphngrtz <stephan.goertz@gmail.com>
2015-12-16 18:00:30 +0100

    initial setup & embedding example

---
+++ embedding/src/main/java/de/stphngrtz/hellovertx/Main.java
@@ -0,0 +1,13 @@
+package de.stphngrtz.hellovertx;
+
+import io.vertx.core.Vertx;
+
+public class Main {
+
+    /**
+     * https://github.com/vert-x3/vertx-examples/blob/master/core-examples/README.adoc#embedding
+     */
+    public static void main(String[] args) {
+        Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+    }
+}
